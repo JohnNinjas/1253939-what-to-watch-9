d37871effa1ca0b50632220eb86cadeca1120bbb
imenchenkov <imenchenkov@livemaster.ru>
2022-02-12 16:29:57 +0300

    2.11. начинаем программировать: сделано до пункта 11

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const promoFilmParams = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  releaseDate: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title={promoFilmParams.title} genre={promoFilmParams.genre} releaseDate={promoFilmParams.releaseDate} />
   </React.StrictMode>,
   document.getElementById('root'));
