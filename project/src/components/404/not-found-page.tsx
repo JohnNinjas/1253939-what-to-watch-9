f6367789f453a2a3f1b9c7ec67af1a78f3f7c5c9
imenchenkov <imenchenkov@livemaster.ru>
2022-02-15 17:04:51 +0300

    3.5. Маршрут по тайным тропам: стилизация страницы 404

--- project/src/components/404/not-found-page.tsx
+++ project/src/components/404/not-found-page.tsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
+
 function NotFoundPage(): JSX.Element {
   return (
-    <>
+    <div style={{textAlign: 'center'}}>
       <h1>404 not found</h1>
       <h2>Страница не найдена</h2>
-    </>
+      <Link to='/'>
+        <span>Вернуться на главную</span>
+      </Link>
+    </div>
   );
 }
 
