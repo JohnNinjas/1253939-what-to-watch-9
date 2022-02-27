ad3b1066d19f602bb85e9c390bbaa7985623e569
imenchenkov <imenchenkov@livemaster.ru>
2022-02-27 10:05:44 +0300

    5.15. ольше подробностей (часть 1)

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,28 +1,13 @@
 import { Film } from '../../types/film';
-import { Link } from 'react-router-dom';
+import VideoPlayer from '../video-player/video-player';
 
 type FilmCardProps = {
   film: Film,
-  isActive: boolean,
-  onHover: (id: number | null) => void,
 };
 
-function FilmCard({film, isActive, onHover}: FilmCardProps): JSX.Element {
+function FilmCard({film}: FilmCardProps): JSX.Element {
   return (
-    <article
-      className="small-film-card catalog__films-card"
-      onMouseEnter={() => onHover(film.id)}
-      onMouseLeave={() => onHover(null)}
-    >
-      <div className="small-film-card__image">
-        <img src={film.previewImg} alt={film.title} width="280" height="175"/>
-      </div>
-      <h3 className="small-film-card__title">
-        <Link className="small-film-card__link" to={`/films/${film.id}`}>
-          {film.title}
-        </Link>
-      </h3>
-    </article>
+    <VideoPlayer autoPlay={false} film={film} />
   );
 }
 
